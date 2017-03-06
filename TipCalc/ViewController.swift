87d6933fabbbadc257e671af172684f4ec681a76
Sanampudi, Manohar <sanamm@usxxsanammm1.local>
2017-03-06 09:31:41 -0800

    Init version

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("loading view...")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        let tipPcnt = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPcnt[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func pcntChanged(_ sender: Any) {
+        calcTip(sender)
+    }  
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        let userPrefs = UserDefaults.standard
+        self.tipControl.selectedSegmentIndex = userPrefs.integer(forKey: "defTipIndx")
+        calcTip(animated)
+    }
+    
 }
 
